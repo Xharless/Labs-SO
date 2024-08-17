b3974c4bebf948455f4b5b1647dcd49996b5fc2a
Xharless <carlosarevalo1702@hotmail.com>
2024-08-16 22:51:17 -0400

    Funciona para 1 archivo

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -10,29 +10,25 @@
 
 using namespace std;
 
-
-const string path = "./archivos_deportes/"; // los dos puntos es porque el .exe en windows se crea en la caroeta de output, entonces tiene que salir una mas arriba
+const string path = "./archivos_deportes/";
 
 void abrir_archivo();
 void leer_archivo(const string&);
-void crear_carpetas(const string&, const string&, const string& , const string&);
-void crear_directorio(const string&);
+void crear_carpetas(const string&, const string&, const string&, const string&);
+void crear_directorio_recursivo(const string&);
 
 int main(){
     abrir_archivo();
-
-
     return 0;
 }
 
-
 void abrir_archivo(){
     if(DIR* pDIR = opendir(path.c_str())){ 
-        cout << "DIRECTORIO ENCONTRADO " << endl;
+        cout << "DIRECTORIO ENCONTRADO" << endl;
         while(dirent* entry = readdir(pDIR)){
             string fileName = entry->d_name;  
             if(fileName != "." && fileName != ".."){  
-                cout<<fileName<<endl;
+                cout << "Leyendo archivo: " << fileName << endl;
                 leer_archivo(fileName);
             }
         }  
@@ -52,47 +48,70 @@ void leer_archivo(const string& fileName){
         getline(file, medalla);
         file.close();
 
+        cout << "Archivo leído exitosamente: " << fileName << endl;
+        cout << "Deporte: " << deporte << ", Categoría: " << categoria << ", Medalla: " << medalla << endl;
+
         crear_carpetas(deporte, categoria, fileName, medalla);
 
     } else {
-        cout << "NO SE PUDO ABRIR EL ARCHIVO" << endl;
+        cout << "NO SE PUDO ABRIR EL ARCHIVO: " << filePath << endl;
     }
 }
 
-void crear_carpetas(const string& deporte, const string& categoria, const string& participante , const string& medalla){
+void crear_carpetas(const string& deporte, const string& categoria, const string& participante, const string& medalla){
     string baseD = "Carpeta Actual/" + deporte + "/" + categoria;
-    crear_directorio("Carpeta Actual");
-    crear_directorio(baseD);
+    cout << "Creando directorio base: " << baseD << endl;
+
+    crear_directorio_recursivo(baseD);
 
     string medallaD;
     if((medalla == "Oro") || (medalla == "Plata") || (medalla == "Bronce")){
         medallaD = baseD + "/Con Medalla";
-        crear_directorio(medallaD);
+        crear_directorio_recursivo(medallaD);
         string newfilepath = medallaD + "/" + participante;
-        if (rename((path + "/" + participante).c_str(), newfilepath.c_str()) != 0) {
+        if (rename((path + participante).c_str(), newfilepath.c_str()) != 0) {
             cerr << "Error moviendo el archivo: " << participante << " a " << newfilepath << endl;
+        } else {
+            cout << "Archivo movido a: " << newfilepath << endl;
         }
-
     } else {
         medallaD = baseD + "/Sin Medalla";
-        crear_directorio(medallaD);
+        crear_directorio_recursivo(medallaD);
         string newfilepath = medallaD + "/" + participante;
-        if (rename((path + "/" + participante).c_str(), newfilepath.c_str()) != 0) {
+        if (rename((path + participante).c_str(), newfilepath.c_str()) != 0) {
             cerr << "Error moviendo el archivo: " << participante << " a " << newfilepath << endl;
+        } else {
+            cout << "Archivo movido a: " << newfilepath << endl;
         }
     }
-    
 }
 
-void crear_directorio(const string& dirPath ){
-    struct stat info;
-    if (stat(dirPath.c_str(), &info) != 0){
-        #if defined(_WIN32) || defined(_WIN64)
-        if (_mkdir(dirPath.c_str()) != 0) {
-        #else
-        if (mkdir(dirPath.c_str(), 0777) != 0) {
-        #endif
-            cerr << "No se pudo crear el directorio: " << dirPath << endl;
+void crear_directorio_recursivo(const string& dirPath ){
+    size_t pos = 0;
+    string tempPath = "";
+    
+    while ((pos = dirPath.find('/', pos)) != string::npos) {
+        tempPath = dirPath.substr(0, pos);
+        if (!tempPath.empty()) {
+            #if defined(_WIN32) || defined(_WIN64)
+            if (_mkdir(tempPath.c_str()) != 0 && errno != EEXIST) {
+            #else
+            if (mkdir(tempPath.c_str(), 0777) != 0 && errno != EEXIST) {
+            #endif
+                cerr << "No se pudo crear el directorio: " << tempPath << endl;
+                return;
+            }
         }
+        pos++;
+    }
+
+    #if defined(_WIN32) || defined(_WIN64)
+    if (_mkdir(dirPath.c_str()) != 0 && errno != EEXIST) {
+    #else
+    if (mkdir(dirPath.c_str(), 0777) != 0 && errno != EEXIST) {
+    #endif
+        cerr << "No se pudo crear el directorio: " << dirPath << endl;
+    } else {
+        cout << "Directorio creado: " << dirPath << endl;
     }
 }
