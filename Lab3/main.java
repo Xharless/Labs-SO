a81bad19528450b5caa71319041ca1b00b479c00
Daniel Maturana <matucristi@gmail.com>
2024-11-02 14:04:03 -0300

    añadido ejemplo de hilos

--- Lab3/main.java
+++ Lab3/main.java
@@ -1,8 +1,39 @@
-//compila: javac main.java
-//ejecuta: java main
+/*
+compila: javac main.java
+ejecuta: java main
+*/
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
-public class HelloWorld {
+//Ejemplo de hilos
+class MiHilo extends Thread {
+    private String nombre;
+
+    public MiHilo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(nombre + " ejecutando iteración " + i);
+            try {
+                Thread.sleep(500); // Pausa de medio segundo (500 ms)
+            } catch (InterruptedException e) {
+                System.out.println(nombre + " fue interrumpido");
+            }
+        }
+        System.out.println(nombre + " ha terminado su ejecución.");
+    }
+}
+
+
+public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        MiHilo hilo1 = new MiHilo("Hilo 1");
+        MiHilo hilo2 = new MiHilo("Hilo 2");
+
+        hilo1.start(); // Iniciar Hilo 1
+        hilo2.start(); // Iniciar Hilo 2
     }
 }
