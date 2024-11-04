e8dd83827d3528ce8d9dc2400ec77744b11ff3b8
Xharless <carlosarevalo1702@hotmail.com>
2024-11-04 14:49:18 -0300

    lectura de todos los archivos de los casos, ademas de arreglar las extenciones a .txt para los del caso 1

--- Lab3/main.java
+++ Lab3/main.java
@@ -2,13 +2,16 @@
 compila: javac main.java
 ejecuta: java main
 */
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveTask;
-import java.io.BufferedReader; 
+
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException; 
 import java.io.FileReader; 
 import java.io.IOException; 
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
 //Ejemplo de hilos
 class MiHilo extends Thread {
@@ -31,21 +34,44 @@ class MiHilo extends Thread {
         System.out.println(nombre + " ha terminado su ejecución.");
     }
 }
-
-
 public class main {
-    // funcion para leer lo que contiene los archivos
-    public static void muestraContenido(String archivo) throws FileNotFoundException, IOException{
-        String cadena;
-        FileReader f = new FileReader(archivo);
-        BufferedReader b = new BufferedReader(f);
-        while ((cadena = b.readLine()) != null) {
-            System.out.println(cadena);
+    // funcion que lee todas las direcciones de la carpeta (en este caso es Caso1)
+    public static String[] getFiles(String dir_path) throws FileNotFoundException, IOException{
+        String[] arr_res = null;
+        File f = new File(dir_path);
+        if(f.isDirectory()){
+            List<String> res = new ArrayList<>();
+            File[] arr_content = f.listFiles();
+            int size = arr_content.length;
+            for(int i = 0; i<size; i++){
+                if(arr_content[i].isFile())
+                res.add(arr_content[i].toString());
+            }
+            arr_res = res.toArray(new String[0]);
+        } else {
+            System.err.println("Path no valido");
         }
-        b.close();
+        return arr_res;
     }
-    public static void main(String[] args) {
-        muestraContenido("Lab3/archivos_prueba/Caso1/code1.txt");
+
+    public static void main(String[] args) throws IOException {
+        String path = "archivos_prueba/Caso1"; // aqui hay que cambiar el nombre del caso para que lea el siguiebte
+        String[] files = getFiles(path);
+        if(files != null ){
+            for(String filePath : files){
+                System.out.println("Leyendo archivo: " + filePath);
+                try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+                    String linea;
+                    while((linea = br.readLine()) != null){
+                        System.out.println(linea);
+                    }
+                } catch (IOException e){
+                    System.err.println("Error al leer el archivo: " + filePath );
+                    e.printStackTrace();
+                }
+                System.out.println("---------Fin del archivo---------\n");
+            }
+        }
         MiHilo hilo1 = new MiHilo("Hilo 1");
         MiHilo hilo2 = new MiHilo("Hilo 2");
 
--- Lab3/pruebas.java
+++ Lab3/pruebas.java
@@ -1,18 +1,46 @@
-import java.io.BufferedReader; 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException; 
 import java.io.FileReader; 
 import java.io.IOException; 
+import java.util.List;
+import java.util.ArrayList;
+
 public class pruebas {
-    public static void muestraContenido(String archivo) throws FileNotFoundException, IOException{
-        String cadena;
-        FileReader f = new FileReader(archivo);
-        BufferedReader b = new BufferedReader(f);
-        while ((cadena = b.readLine()) != null) {
-            System.out.println(cadena);
+    public static String[] getFiles(String dir_path) throws FileNotFoundException, IOException{
+        String[] arr_res = null;
+        File f = new File(dir_path);
+        if(f.isDirectory()){
+            List<String> res = new ArrayList<>();
+            File[] arr_content = f.listFiles();
+            int size = arr_content.length;
+            for(int i = 0; i<size; i++){
+                if(arr_content[i].isFile())
+                res.add(arr_content[i].toString());
+            }
+            arr_res = res.toArray(new String[0]);
+        } else {
+            System.err.println("Path no valido");
         }
-        b.close();
+        return arr_res;
     }
     public static void main(String[] args) throws IOException {
-        muestraContenido("Lab3/archivos_prueba/Caso1/code1.txt");
+        String path = "archivos_prueba/Caso1";
+        String[] files = getFiles(path);
+        if(files != null ){
+            for(String filePath : files){
+                System.out.println("Leyendo archivo: " + filePath);
+                try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+                    String linea;
+                    while((linea = br.readLine()) != null){
+                        System.out.println(linea);
+                    }
+                } catch (IOException e){
+                    System.err.println("Error al leer el archivo: " + filePath );
+                    e.printStackTrace();
+                }
+                System.out.println("---------Fin del archivo---------\n");
+            }
+        }
     }
 }
