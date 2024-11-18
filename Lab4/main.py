d8fb109dc2c6f27c2859b7c7a8ba562134692de6
carlos <carlosarevalo1702@hotmail.com>
2024-11-18 20:38:01 -0300

    validacion

--- Lab4/main.py
+++ Lab4/main.py
@@ -2,6 +2,30 @@ import threading
 import time
 import random
 
+capacidad_por_zona = 32 
+semaforo_zona = threading.Semaphore(capacidad_por_zona)
+
+
+lock_registro = threading.Lock() #Para proteger el acceso al archivo
+registro = "Validacion.txt"
+
+
+def validar_jugador(id_jugador):
+    with semaforo_zona:
+        inicio = time.strftime("%H:%M:%S") #hora inicio que lo piden en el archivo final
+        print(f"Jugador {id_jugador} iniciio validacion a las {inicio}")
+        time.sleep(random.randint(1,3))
+
+        resultado = "Validacion completa" if random.random() > 0.2 else "Validacion fallida"
+        fin = time.strftime("%H:%M:%S")
+        print(f"Jugador {id_jugador} termina validacion a las {fin} con resultado: {resultado}")
+
+        #escribirlos en el registro
+        with lock_registro:
+            with open(registro, 'a') as archivo:
+                archivo.write(f"{id_jugador}, {inicio}, {resultado}\n")
+
+
 
 # comportamiento de un jugador
 def jugador(id_jugador):
