21f81cd11f906c7abd0f06078ad3c13a47e68a2f
Daniel Maturana <matucristi@gmail.com>
2024-11-23 18:38:17 -0300

    arreglar sincronizacion entre fases, ejecución correcta de la logica  entre rondas

--- Lab4/main.py
+++ Lab4/main.py
@@ -3,15 +3,19 @@ import threading
 import time
 import random
 
-LOCK_REGISTRO = threading.Lock()  # Para proteger el acceso al archivo
+LOCK_REGISTRO = threading.Lock()  #protege acceso al archivo
+CONDICION_FASE = threading.Condition() #protege la ronda
 REGISTRO_VALIDACION = "Validacion.txt"
 REGISTRO_FINAL = "Final.txt"
 
-# Configuración del torneo
+#configuracion del torneo
 NUMERO_JUGADORES = 256
 CAPACIDAD_POR_GRUPO = 32
 DURACION_VALIDACION = 15
 DURACION_ENFRENTAMIENTO = 10
+GANADOR = []
+REPECHAJE = []
+TERMINADO = False
 
 
 def registrar_resultado(nombre_archivo, contenido):
@@ -22,15 +26,15 @@ def registrar_resultado(nombre_archivo, contenido):
 
 def validar_jugador(id_jugador, semaforo_zona):
     with semaforo_zona:
-        # hora inicio
+        #hora inicio
         inicio = time.strftime("%H:%M:%S")
         print(f"Jugador {id_jugador} inició validación a las {inicio}")
 
-        # escribirlos en el registro
+        #escribirlos en el registro
         resultado = "Validacion Completa"
         registrar_resultado(REGISTRO_VALIDACION, f"{id_jugador}, {inicio}, {resultado}")
 
-        # simula validacion esperando 15 segundos
+        #simula validacion esperando 15 segundos
         time.sleep(DURACION_VALIDACION)
 
 
@@ -38,25 +42,23 @@ def procesar_grupo(grupo, semaforo):
     hebras = []
 
     for id_jugador in grupo:
-        # una hebra por jugador
+        #una hebra por jugador
         hebra = threading.Thread(target=validar_jugador, args=(id_jugador, semaforo))
         hebras.append(hebra)
         hebra.start()
 
-    # espera a que todos los jugadores del grupo terminen
+    #espera a que todos los jugadores del grupo terminen
     for hebra in hebras:
         hebra.join()
 
 
 def asignar_grupo(capacidad_grupo, num_jugadores, semaforos_zona):
-    # crea los grupos
+    #crea los grupos
     grupos = [list(range(i, i + capacidad_grupo)) for i in range(0, num_jugadores, capacidad_grupo)]
 
-    hebras_zonas = []
     for i, grupo in enumerate(grupos):
-        semaforo = semaforos_zona[i % 2]  # elige semaforo por zona
+        semaforo = semaforos_zona[i % 2]  #elige semaforo por zona
         hebra_zona = threading.Thread(target=procesar_grupo, args=(grupo, semaforo))
-        hebras_zonas.append(hebra_zona)
         hebra_zona.start()
 
 
@@ -67,100 +69,150 @@ def enfrentar_jugadores(jugador1, jugador2):
     return ganador
 
 
-def fase_eliminacion(jugadores, ronda):
-    # Inicializa archivo para los ganadores de la ronda
+def fase_eliminacion_por_ronda(jugadores, ronda):
+    global REPECHAJE
+
+    #inicializa archivo
     archivo_ganadores = f'Ganadores_Ronda{ronda}.txt'
-    # Sobrescribe el archivo si ya existe
     with open(archivo_ganadores, "w") as archivo:
         archivo.write("Enfrentamiento -> Ganador\n")
 
     ganadores = []
-    perdedores = []  # Lista para los perdedores de esta ronda
+    perdedores = []  #lista para los perdedores de esta ronda
     for i in range(0, len(jugadores), 2):
         ganador = enfrentar_jugadores(jugadores[i], jugadores[i + 1])
         ganadores.append(ganador)
-        perdedores.append(jugadores[i] if ganador != jugadores[i] else jugadores[i + 1])  # El perdedor de esta ronda
+        perdedores.append(jugadores[i] if ganador != jugadores[i] else jugadores[i + 1])
 
         registrar_resultado(archivo_ganadores, f"{jugadores[i]} vs {jugadores[i + 1]} -> {ganador}")
 
-    # Guardar los perdedores en un archivo
-    archivo_perdedores = f"Perdedores_Ronda{ronda}.txt"
-    with open(archivo_perdedores, "w") as archivo:
-        archivo.write("Enfrentamiento -> Perdedor\n")
-        for jugador in perdedores:
-            archivo.write(f"{jugador}\n")
+    #se agregan a la ronda de repechaje
+    REPECHAJE += perdedores
 
     time.sleep(DURACION_ENFRENTAMIENTO)
-    return ganadores, perdedores  # Regresamos también los perdedores
+    if ronda == 1:
+        return ganadores, perdedores
+    return ganadores
 
 
-def fase_repechaje(losers, ronda):
+def fase_repechaje_por_ronda(losers, ronda):
+    global REPECHAJE
+
+    #inicializa archivo
     archivo_repechaje = f"Perdedores_Ronda{ronda}.txt"
     with open(archivo_repechaje, "w") as archivo:
         archivo.write("Enfrentamiento -> Ganador\n")
 
-    ganadores_repechaje = []
-    i = 0
-    while i < len(losers) - 1:  # Procesa de dos en dos mientras haya suficientes jugadores
+    ganadores = []
+    perdedores = []
+    for i in range(0, len(losers), 2):
         ganador = enfrentar_jugadores(losers[i], losers[i + 1])
-        ganadores_repechaje.append(ganador)
+        ganadores.append(ganador)
+        perdedores.append(losers[i] if ganador != losers[i] else losers[i + 1])
+
         registrar_resultado(archivo_repechaje, f"{losers[i]} vs {losers[i + 1]} -> {ganador}")
-        i += 2
 
-    # Si queda un jugador sin pareja, pasa automáticamente a la siguiente ronda
-    if len(losers) % 2 != 0:
-        print(f"{losers[-1]} pasa automáticamente a la siguiente ronda.")
-        ganadores_repechaje.append(losers[-1])
-        registrar_resultado(archivo_repechaje, f"{losers[-1]} -> Pasa automáticamente")
-
+    #se eliminan los perdedores
     time.sleep(DURACION_ENFRENTAMIENTO)
-    return ganadores_repechaje
+    if ronda != 1:     
+        REPECHAJE = [item for item in REPECHAJE if item not in perdedores]
+    else:
+        return ganadores
 
 
-# Elimina si ya existen los archivos
+
+def fase_eliminacion(ganadores_eliminacion):
+    global GANADOR, TERMINADO
+
+    with CONDICION_FASE:
+        ronda = 2
+        cant_jugadores = len(ganadores_eliminacion)
+        ganadores_ronda_eliminacion = ganadores_eliminacion
+
+        while cant_jugadores > 1:
+            while not TERMINADO:
+                CONDICION_FASE.wait()
+
+            #comienza la ronda
+            TERMINADO = False
+            print(f"Comienza la ronda {ronda} con {cant_jugadores} jugadores en eliminación")
+            ganadores = fase_eliminacion_por_ronda(ganadores_ronda_eliminacion, ronda)
+            ganadores_ronda_eliminacion = ganadores  #los que ganan van a la siguiente ronda
+
+            #terminamos la ronda
+            ronda += 1
+            cant_jugadores -= len(ganadores)
+            TERMINADO = True
+            CONDICION_FASE.notify_all()
+
+        GANADOR = ganadores_ronda_eliminacion
+        print(f"Termina fase de eliminación con {ronda-1} rondas")
+
+
+def fase_repechaje():
+    global REPECHAJE, TERMINADO
+
+    with CONDICION_FASE:
+        ronda_repechaje = 2
+        cant_jugadores = len(REPECHAJE)
+
+        while cant_jugadores > 1:
+            while TERMINADO:
+                CONDICION_FASE.wait()
+
+            #comienza la ronda
+            TERMINADO = False
+            ganadores_ronda_repechaje = REPECHAJE
+            print(f"Comienza la ronda {ronda_repechaje} de perdedores con {cant_jugadores} jugadores")
+            fase_repechaje_por_ronda(ganadores_ronda_repechaje, ronda_repechaje)
+
+            #termina la ronda
+            ronda_repechaje += 1
+            cant_jugadores = len(REPECHAJE)
+            TERMINADO = True
+            CONDICION_FASE.notify_all()
+        print(f"Termina fase de repechaje con {ronda_repechaje-1} rondas")
+
+
+#elimina archivos en caso de que existan
 if REGISTRO_VALIDACION in os.listdir("."):
     os.remove(f"./{REGISTRO_VALIDACION}")
 if REGISTRO_FINAL in os.listdir("."):
     os.remove(f"./{REGISTRO_FINAL}")
 
-# Escribe primera línea como formato
+#escribe formato de validacion
 with open(REGISTRO_VALIDACION, 'w') as archivo_validacion:
     archivo_validacion.write("ID_Jugador, Hora_Inicio, Resultado\n")
 
-# Semáforos_zona permiten que entren dos grupos
+#semaforos para dos grupos
 semaforos_zona = [threading.Semaphore(CAPACIDAD_POR_GRUPO), threading.Semaphore(CAPACIDAD_POR_GRUPO)]
 
-# Validación inicial
+#se validan jugadores por grupo
 asignar_grupo(CAPACIDAD_POR_GRUPO, NUMERO_JUGADORES, semaforos_zona)
 jugadores_validos = [f"Jugador{i}" for i in range(1, NUMERO_JUGADORES + 1)]
 
-# Fase de eliminación directa
-ganadores_ronda = jugadores_validos
-ronda = 1
-cant_jugadores = len(jugadores_validos)
-while cant_jugadores > 1:
-    print(f"Comienza la ronda {ronda} con {cant_jugadores} jugadores")
-    ganadores_eliminacion, perdedores_eliminacion = fase_eliminacion(ganadores_ronda, ronda)
-    ganadores_ronda = ganadores_eliminacion  # Los que avanzan a la siguiente ronda
-    ronda += 1
-    cant_jugadores -= len(ganadores_eliminacion)
-print(f"Termina fase de eliminación con {ronda} rondas")
+#juegan la primera ronda y se crean ambos torneos
+ganadores_eliminacion, perdedores_eliminacion = fase_eliminacion_por_ronda(jugadores_validos, 1)
+ganadores_repechaje = fase_repechaje_por_ronda(perdedores_eliminacion, 1)
+REPECHAJE += ganadores_repechaje
 
-# Fase de repechaje (solo los perdedores)
-losers_ronda = perdedores_eliminacion  # Solo los perdedores
-ronda_repechaje = 1
-cant_perdedores = len(losers_ronda)
-while cant_perdedores > 1:
-    print(f"Comienza la ronda {ronda_repechaje} de perdedores con {cant_perdedores} jugadores")
-    ganadores_repechaje = fase_repechaje(losers_ronda, ronda_repechaje)
-    losers_ronda = ganadores_repechaje  # Los que avanzan en el repechaje
-    ronda_repechaje += 1
-    cant_perdedores -= len(ganadores_repechaje)
-print(f"Termina fase de repechaje con {ronda_repechaje} rondas")
+#fase eliminacion activa siempre y cuando termina la ronda manda señal a la de repechaje para que continue con la cantidad sumada de jugadores
+hebras_fases = []
+hebra_fase_eliminacion = threading.Thread(target=fase_eliminacion, args=(ganadores_eliminacion,))
+hebras_fases.append(hebra_fase_eliminacion)
+hebra_fase_eliminacion.start()
 
-# Ronda final
-finalista_eliminacion = ganadores_ronda
-finalista_repechaje = losers_ronda
+hebra_fase_repechaje = threading.Thread(target=fase_repechaje)
+hebras_fases.append(hebra_fase_repechaje)
+hebra_fase_repechaje.start()
+
+for hebra in hebras_fases:
+    hebra.join()
+
+
+#ronda final
+finalista_eliminacion = GANADOR
+finalista_repechaje = REPECHAJE
 ganador_final = enfrentar_jugadores(finalista_eliminacion, finalista_repechaje)
 
 with open(REGISTRO_FINAL, "w") as archivo_final:
